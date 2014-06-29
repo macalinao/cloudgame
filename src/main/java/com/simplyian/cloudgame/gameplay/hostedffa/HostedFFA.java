34a76c47cd8ba61ba9188c4b4d1aa4469694682e
Ian Macalinao <ian@giza.us>
2014-06-29 16:27:34 -0500

    Add HostedFFA
    Signed-off-by: Ian Macalinao <ian@giza.us>

---
+++ src/main/java/com/simplyian/cloudgame/gameplay/hostedffa/HostedFFA.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.simplyian.cloudgame.gameplay.hostedffa;
+
+import com.simplyian.cloudgame.CloudGame;
+import com.simplyian.cloudgame.game.Game;
+import com.simplyian.cloudgame.gameplay.Gameplay;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+
+/**
+ *
+ * @author ian
+ */
+public abstract class HostedFFA<T extends HostedFFAState> extends Gameplay<T> {
+
+    private Game<T> game;
+
+    private Set<UUID> prizes = new HashSet<>();
+
+    protected HostedFFA(CloudGame plugin, String name) {
+        super(plugin, name);
+    }
+
+    public Game<T> getGame() {
+        return game;
+    }
+
+    public void setGame(Game<T> game) {
+        this.game = game;
+    }
+
+    /**
+     * Adds the player to the list of people who deserve prizes.
+     *
+     * @param p
+     */
+    public void addPrize(Player p) {
+        prizes.add(p.getUniqueId());
+    }
+
+    /**
+     * Tries to redeem a prize.
+     *
+     * @param p
+     * @return
+     */
+    public boolean redeemPrize(Player p) {
+        if (!prizes.contains(p.getUniqueId())) {
+            return false;
+        }
+        prizes.remove(p.getUniqueId());
+        Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "ccrates give 3 " + p.getName() + " 3");
+        return true;
+    }
+}
