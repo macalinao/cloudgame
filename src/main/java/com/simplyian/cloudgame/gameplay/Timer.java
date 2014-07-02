50f16ac4ebb6bb61468b17a987e5b11a6d232d06
DziNeIT <o.dzineit@gmail.com>
2014-07-02 22:14:39 +0100

    generic Timer class

---
+++ src/main/java/com/simplyian/cloudgame/gameplay/Timer.java
@@ -0,0 +1,72 @@
+package com.simplyian.cloudgame.gameplay;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import com.simplyian.cloudgame.events.GameStartEvent;
+import com.simplyian.cloudgame.game.Game;
+
+public abstract class Timer<T extends State> extends BukkitRunnable {
+    private final Game<T> game;
+
+    private final Map<Integer, String> messages;
+
+    private final List<Integer> times;
+
+    private final long start;
+
+    private final int length;
+
+    private int current = 0;
+
+    public Timer(Game<T> game, Map<Integer, String> messages) {
+        this.game = game;
+        this.messages = messages;
+
+        times = new ArrayList<>(messages.keySet());
+        length = times.get(times.size() - 1);
+        start = System.currentTimeMillis();
+    }
+
+    @Override
+    public void run() {
+        if (game.getState().isStarted() || game.getState().isOver()) {
+            cancel();
+            return;
+        }
+
+        // Expect that the times are in length order
+        int secsRemaining = length - (((int) (System.currentTimeMillis() - start)) / 1000);
+
+        if (secsRemaining <= 0) {
+            Bukkit.getPluginManager().callEvent(new GameStartEvent(game));
+            onEnd();
+            cancel();
+        } else if (secsRemaining <= times.get(current)) {
+            announceTime(messages.get(current));
+            current++;
+        }
+    }
+
+    public void onEnd() {
+    }
+
+    /**
+     * Default announcement message. Can be overriden in subclasses
+     * 
+     * @param time
+     */
+    protected void announceTime(String time) {
+        for (Player p : Bukkit.getOnlinePlayers()) {
+            game.getGameplay().sendBanner(p,
+                    "A " + game.getGameplay().getName() + " on map $D" + game.getArena().getName() + " $Lis starting in $D" + time + "$L!",
+                    "Type $D/" + game.getGameplay().getId() + " join $Lto join $D" + game.getState().getPlayers().size() + " $Lother players! ");
+        }
+    }
+}
--- src/main/java/com/simplyian/cloudgame/gameplay/hostedffa/HostedFFAAnnouncerTask.java
+++ src/main/java/com/simplyian/cloudgame/gameplay/hostedffa/HostedFFAAnnouncerTask.java
@@ -5,61 +5,42 @@
  */
 package com.simplyian.cloudgame.gameplay.hostedffa;
 
-import com.simplyian.cloudgame.events.GameStartEvent;
-import com.simplyian.cloudgame.game.Game;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
-import org.bukkit.scheduler.BukkitRunnable;
+
+import com.simplyian.cloudgame.events.GameStartEvent;
+import com.simplyian.cloudgame.game.Game;
+import com.simplyian.cloudgame.gameplay.Timer;
 
 /**
  * Announces the game and starts it when the time is up.
  *
  * @param <T>
  */
-public class HostedFFAAnnouncerTask<T extends HostedFFAState> extends BukkitRunnable {
+public class HostedFFAAnnouncerTask<T extends HostedFFAState> extends Timer<T> {
+    private static final Map<Integer, String> messages = new HashMap<>();
+
+    static {
+        messages.put(5 * 60, "5 minutes");
+        messages.put(3 * 60, "3 minutes");
+        messages.put(1 * 60, "1 minute");
+        messages.put(30, "30 seconds");
+        messages.put(10, "10 seconds");
+    }
 
     private final Game<T> game;
 
-    private final long start = System.currentTimeMillis();
-
-    private int announceCount = 0;
-
     public HostedFFAAnnouncerTask(Game<T> game) {
+        super(game, messages);
         this.game = game;
     }
 
     @Override
-    public void run() {
-        if (game.getState().isStarted() || game.getState().isOver()) {
-            cancel();
-            return;
-        }
-
-        int secsLeft = 5 * 60 - (((int) (System.currentTimeMillis() - start)) / 1000);
-
-        if (secsLeft <= 5 * 60 && announceCount == 0) {
-            announceTime("5 minutes");
-            announceCount++;
-        } else if (secsLeft <= 3 * 60 && announceCount == 1) {
-            announceTime("3 minutes");
-            announceCount++;
-        } else if (secsLeft <= 1 * 60 && announceCount == 2) {
-            announceTime("1 minute");
-            announceCount++;
-        } else if (secsLeft <= 30 && announceCount == 3) {
-            announceTime("30 seconds");
-            announceCount++;
-        } else if (secsLeft <= 10 && announceCount == 4) {
-            announceTime("10 seconds");
-            announceCount++;
-        } else if (secsLeft <= 0 && announceCount == 5) {
-            Bukkit.getPluginManager().callEvent(new GameStartEvent(game));
-            cancel();
-        }
-    }
-
-    private void announceTime(String time) {
+    protected void announceTime(String time) {
         for (Player p : Bukkit.getOnlinePlayers()) {
             game.getGameplay().sendBanner(p,
                     "A " + game.getGameplay().getName() + " on map $D" + game.getArena().getName() + " $Lis starting in $D" + time + "$L!",
