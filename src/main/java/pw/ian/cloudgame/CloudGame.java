6e23938327b8a4f0731ffee34103a4abfc605f6c
Ian Macalinao <ian@giza.us>
2014-07-30 17:08:45 -0500

    Register game command
    Signed-off-by: Ian Macalinao <ian@giza.us>

--- src/main/java/pw/ian/cloudgame/CloudGame.java
+++ src/main/java/pw/ian/cloudgame/CloudGame.java
@@ -5,6 +5,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 import pw.ian.albkit.command.Commands;
 import pw.ian.cloudgame.commands.arena.ArenaCommand;
+import pw.ian.cloudgame.commands.game.GameCommand;
 import pw.ian.cloudgame.game.GameManager;
 import pw.ian.cloudgame.gameplay.Gameplay;
 import pw.ian.cloudgame.gameplay.GameplayManager;
@@ -32,6 +33,7 @@ public class CloudGame extends JavaPlugin {
         i = this;
 
         Commands.registerCommand(this, new ArenaCommand(this));
+        Commands.registerCommand(this, new GameCommand(this));
 
         modelManager = new ModelManager(this);
         modelManager.load();
